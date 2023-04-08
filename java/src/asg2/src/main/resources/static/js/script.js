0190745cc723ad8568461fcc7be44277e418d561
AlexLeungZ <alexleungchunwai@gmail.com>
2023-04-09 01:39:36 +0800

    Update for frontend basic

--- java/src/asg2/src/main/resources/static/js/script.js
+++ java/src/asg2/src/main/resources/static/js/script.js
@@ -9,22 +9,48 @@ var app = new Vue({
 		},
 	},
 	async mounted() {
-		this.getTodo();
+		await this.getTodo();
 	},
 	methods: {
-		getTodo() {
-			fetch("http://192.168.10.182:3008/api/todo").then((response) => {
-				response.json().then((data) => {
-					console.log(data);
-					app.rows = data.map((movie) => {
-						return {
-							id: movie.id,
-							name: movie.name,
-							done: movie.done,
-						};
-					});
-				});
-			});
+		async getTodo() {
+			const res = await fetch("http://192.168.10.182:3008/api/todo");
+			const json = await res.json();
+			this.rows = json;
+		},
+
+		editTodo(row) {
+			this.editForm.id = row.id;
+			this.editForm.name = row.name;
+			this.editForm.done = row.done;
+		},
+
+		async saveTodo() {
+			const settings = {
+				method: "PUT",
+				headers: { Accept: "application/json", "Content-Type": "application/json" },
+				body: JSON.stringify(this.editForm),
+			};
+
+			const post = await fetch(`http://192.168.10.182:3008/api/todo/${this.editForm.id}`, settings);
+			await this.reloadRow(post.status);
+			location.reload();
+		},
+
+		async deleteTodo(row) {
+			const settings = {
+				method: "DELETE",
+			};
+
+			const del = await fetch(`http://192.168.10.182:3008/api/todo/${row.id}`, settings);
+			await this.reloadRow(del.status);
+		},
+
+		async reloadRow(status) {
+			if (status === 200) {
+				const res = await fetch("http://192.168.10.182:3008/api/todo");
+				const json = await res.json();
+				this.rows = json;
+			}
 		},
 	},
 });
