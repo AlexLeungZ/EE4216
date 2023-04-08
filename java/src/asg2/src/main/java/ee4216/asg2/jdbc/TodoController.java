6584fda27734d44a2b8683996c2d47c8d5816bb1
AlexLeungZ <alexleungchunwai@gmail.com>
2023-04-08 21:27:01 +0800

    Remove unused lib

--- java/src/asg2/src/main/java/ee4216/asg2/jdbc/TodoController.java
+++ java/src/asg2/src/main/java/ee4216/asg2/jdbc/TodoController.java
@@ -2,10 +2,7 @@ package ee4216.asg2.jdbc;
 
 import java.util.List;
 
-import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
--- java/src/asg2/src/main/java/ee4216/asg2/jdbc/TodoDao.java
+++ java/src/asg2/src/main/java/ee4216/asg2/jdbc/TodoDao.java
@@ -2,8 +2,6 @@ package ee4216.asg2.jdbc;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
