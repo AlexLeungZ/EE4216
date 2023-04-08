b8e4f50478dd23d83f482384d1f6b7308c44deda
AlexLeungZ <alexleungchunwai@gmail.com>
2023-04-08 23:30:13 +0800

    Add support for file base database

--- java/src/asg2/src/main/resources/data.sql
+++ java/src/asg2/src/main/resources/data.sql
@@ -1,6 +1,26 @@
-INSERT INTO todo (name, done) VALUES
+-- INSERT INTO todo (name, done) VALUES
+-- ('The 1st Item', 1),
+-- ('The 2nd Item', 1),
+-- ('The 3rd Item', 1),
+-- ('The 4th Item', 0),
+-- ('The 5th Item', 0);
+
+CREATE TEMPORARY TABLE temp_todo (
+    name VARCHAR(128) NOT NULL, 
+    done BIT NOT NULL
+);
+
+INSERT INTO temp_todo (name, done) VALUES
 ('The 1st Item', 1),
 ('The 2nd Item', 1),
 ('The 3rd Item', 1),
 ('The 4th Item', 0),
 ('The 5th Item', 0);
+
+INSERT INTO todo (name, done)
+SELECT t.name, t.done
+FROM temp_todo t
+LEFT JOIN todo ON 1=1
+WHERE todo.id IS NULL;
+
+DROP TABLE IF EXISTS temp_todo;
